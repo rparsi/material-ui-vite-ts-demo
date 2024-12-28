9c90afcc530af0393fcfb261c50beb517174250d
Rahi Parsi <rahi.parsi@gmail.com>
2024-12-27 20:25:59 -0500

    dev: added unit test for generateId method

---
+++ src/component/uuid/test.spec.ts
@@ -0,0 +1,23 @@
+import { afterEach, describe, expect, it, vi } from 'vitest';
+import { nanoid } from 'nanoid';
+import { generateId } from '.';
+
+const id = 'test-id';
+
+vi.mock('nanoid', () => {
+    const mockedNanoid = vi.fn();
+
+    return { nanoid: mockedNanoid };
+});
+
+describe('Testing uuid generation', () => {
+    afterEach(() => {
+        vi.restoreAllMocks();
+    });
+  
+    it('should return "test-id"', () => {
+        vi.mocked(nanoid).mockReturnValue(id);
+        expect(generateId()).toEqual(id);
+        expect(nanoid).toHaveBeenCalledOnce();
+    })
+});
