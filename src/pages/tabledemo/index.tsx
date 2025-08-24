75e0e838785ac4de70e75cf5bb6aec4ad8959c65
Rahi Parsi <rahi.parsi@gmail.com>
2025-08-24 17:55:52 -0400

    dev: added x-data-grid to tabledemo page

--- src/pages/tabledemo/index.tsx
+++ src/pages/tabledemo/index.tsx
@@ -1,10 +1,30 @@
+import { JSX } from 'react';
 import Page from '../../component/page';
+import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid';
 
-export default function TableDemoPage() {
+const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID' },
+    { field: 'col1', headerName: 'Column 1', editable: true, sortable: true },
+    { field: 'col2', headerName: 'Column 2', editable: true, sortable: true }
+];
+
+const rows: GridRowsProp = [
+    { id: 1, col1: 'column 1 value here', col2: 'column 2 value here' },
+    { id: 2, col1: 'column 1 value here', col2: 'column 2 value here' },
+    { id: 3, col1: 'column 1 value here', col2: 'column 2 value here' }
+];
+
+export default function TableDemoPage(): JSX.Element {
     return (
         <Page title='Table Demo'>
             Table demo here.
             <p>next line</p>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                checkboxSelection
+                disableRowSelectionOnClick
+            />
         </Page>
     );
 }
